df5d03b48740cdf8f2a2b5b047ca6168e9c490a4
gukubiack <kubiack.dg@gmail.com>
2024-04-11 17:14:34 -0300

    Integrate with backend

--- script.js
+++ script.js
@@ -15,3 +15,43 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+document.getElementById('btn-enviar').addEventListener('click', function(e) {
+    e.preventDefault()
+    var nome = document.getElementById('nome').value;
+    var email = document.getElementById('email').value;
+
+    var formData = new FormData();
+    formData.append('name', nome);
+    formData.append('email', email);
+
+    fetch('http://localhost:8000/send-email/', {
+        method: 'POST',
+        body: formData, 
+    })
+    .then(response => {
+        if (response.ok) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Sucesso!',
+                text: 'Seu e-mail foi enviado com sucesso.',
+
+            });
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Erro!',
+                text: 'Houve um erro ao enviar o e-mail. Por favor, tente novamente mais tarde.',
+
+            });
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        Swal.fire({
+            icon: 'error',
+            title: 'Erro!',
+            text: 'Houve um erro ao enviar o e-mail. Por favor, tente novamente mais tarde.',
+        });
+    });
+});
